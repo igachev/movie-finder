20862f1b115a2d49ca55e55ad6deea193e99403d
igachev <ivophoenix1135@gmail.com>
2025-05-17 20:35:27 +0300

    created QueryObject that will be used as query params in GetMovies endpoint

---
+++ movies-api/Helpers/QueryObject.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movies_api.Helpers
+{
+    public class QueryObject
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 2;
+    }
+}
