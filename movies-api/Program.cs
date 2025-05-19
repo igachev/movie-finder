b98d6ae844a2be7ed5712d7a3b6d17894540a9d8
igachev <ivophoenix1135@gmail.com>
2025-05-19 15:52:48 +0300

    setup Identity and Authentication in Program.cs

--- movies-api/Program.cs
+++ movies-api/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using movies_api.Data;
 using movies_api.Interfaces;
+using movies_api.Models;
 using movies_api.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +22,33 @@ builder.Services.AddControllers().AddNewtonsoftJson(options => {
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Identity setup
+builder.Services.AddIdentity<AppUser,IdentityRole>(options => {
+    options.Password.RequiredLength = 4;
+}).AddEntityFrameworkStores<ApplicationDBContext>();
+
+// authentication setup
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = 
+    options.DefaultChallengeScheme =
+    options.DefaultForbidScheme =
+    options.DefaultScheme =
+    options.DefaultSignInScheme =
+    options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+        )
+    };
 });
 
 builder.Services.AddScoped<IMovieRepository, MovieRepository>();
