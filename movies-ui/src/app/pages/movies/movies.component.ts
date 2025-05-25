e58accad0104051163d1756d98078ec7b046ff27
igachev <ivophoenix1135@gmail.com>
2025-05-25 14:48:39 +0300

    created getMovies method in MoviesComponent

--- movies-ui/src/app/pages/movies/movies.component.ts
+++ movies-ui/src/app/pages/movies/movies.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal, WritableSignal } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
+import { Movie } from '../../types/MovieTypes';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movies',
@@ -6,6 +9,39 @@ import { Component } from '@angular/core';
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.scss'
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit,OnDestroy {
+    private movieService = inject(MovieService)
+
+    movies: WritableSignal<Movie[]> = signal([]);
+    loading: WritableSignal<boolean> = signal(false);
+    private pageNumber: number = 1;
+    private pageSize: number = 2;
+    private moviesSubscription!: Subscription;
+
+    ngOnInit(): void {
+     this.getMovies()
+    }
+
+    getMovies() {
+      this.loading.set(true)
+      this.moviesSubscription = this.movieService.getMovies(this.pageNumber,this.pageSize).subscribe({
+      next: (res) => {
+        this.movies.set(res)
+        console.log(this.movies())
+      },
+      error: (err) => {
+        console.log(err.error.errors)
+      },
+      complete: () => {
+        this.loading.set(false)
+      }
+    })
+    }
+
+    ngOnDestroy(): void {
+        if(this.moviesSubscription) {
+          this.moviesSubscription.unsubscribe()
+        }
+    }
 
 }
