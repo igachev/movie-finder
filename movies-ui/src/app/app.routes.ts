19a09202f460017a4e7be3eb577a477ecc0682fe
igachev <ivophoenix1135@gmail.com>
2025-05-26 23:08:50 +0300

    created route /users/register

--- movies-ui/src/app/app.routes.ts
+++ movies-ui/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {path:'',component: HomeComponent},
     {path:'movies',component: MoviesComponent},
-    {path:'movies/:id/details',component: MovieDetailsComponent}
+    {path:'movies/:id/details',component: MovieDetailsComponent},
+    {path:'users/register',component: RegisterComponent}
 ];
