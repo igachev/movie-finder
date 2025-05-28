0aca045413574ba7cae558a3aa1bd7bc173a9c47
igachev <ivophoenix1135@gmail.com>
2025-05-28 12:08:09 +0300

    created login component and route  /users/login

--- movies-ui/src/app/app.routes.ts
+++ movies-ui/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {path:'',component: HomeComponent},
     {path:'movies',component: MoviesComponent},
     {path:'movies/:id/details',component: MovieDetailsComponent},
-    {path:'users/register',component: RegisterComponent}
+    {path:'users/register',component: RegisterComponent},
+    {path:'users/login',component: LoginComponent}
 ];
