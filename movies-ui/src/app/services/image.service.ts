adf95a33b7e99d3980b87b117fe7fa2023d1a468
igachev <ivophoenix1135@gmail.com>
2025-08-19 20:28:27 +0300

    created method getImages in ImageService

--- movies-ui/src/app/services/image.service.ts
+++ movies-ui/src/app/services/image.service.ts
@@ -2,6 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+type ImageUrl = {
+  images: string[];
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,6 +17,10 @@ export class ImageService {
 
   uploadImages(formData: FormData,movieTitle: string) {
     return this.http.put<any>(`http://localhost:5174/api/images/upload?movieTitle=${movieTitle}`,formData)
+  }
+
+  getImages(movieTitle: string): Observable<ImageUrl> {
+    return this.http.get<ImageUrl>(`http://localhost:5174/api/images?movieTitle=${movieTitle}`)
   }
 
 }
