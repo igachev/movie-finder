791f6cf1c290541b9a5ed13d47da9edc21f79e84
igachev <ivophoenix1135@gmail.com>
2025-08-25 00:57:23 +0300

    edited FilterMoviesByGenre Component
    1. used ngOnInit method
    2. effect keeps track of pageNumber and currentGenre signals

--- movies-ui/src/app/pages/filter-movies-by-genre/filter-movies-by-genre.component.ts
+++ movies-ui/src/app/pages/filter-movies-by-genre/filter-movies-by-genre.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, signal, WritableSignal } from '@angular/core';
+import { Component, effect, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MovieService } from '../../services/movie.service';
 import { Movie } from '../../types/MovieTypes';
@@ -12,7 +12,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   templateUrl: './filter-movies-by-genre.component.html',
   styleUrl: './filter-movies-by-genre.component.scss'
 })
-export class FilterMoviesByGenreComponent {
+export class FilterMoviesByGenreComponent implements OnInit {
  
   private movieService = inject(MovieService)
   pageNumber = signal(1)
@@ -23,16 +23,22 @@ export class FilterMoviesByGenreComponent {
   constructor() {
     effect(() => {
       this.pageNumber()
+      this.currentGenre()
       if(this.currentGenre() === '') return;
       this.movieService.filterMoviesByGenre(this.pageNumber(),this.pageSize(),this.currentGenre());
     })
+  }
+
+  ngOnInit(): void {
+    this.filteredMovies.set([])
   }
 
 
   onSearch(searchForm: NgForm) {
   const { genreName } = searchForm.value;
   this.currentGenre.set(genreName);
-  this.pageNumber.set(1);
+  if(this.currentGenre() === '') return;
+    this.pageNumber.set(1);
 }
 
 nextPage() {
