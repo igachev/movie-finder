8481a8ef3ff22f35003577c9a4bbee1e29d14b7c
igachev <ivophoenix1135@gmail.com>
2025-09-17 13:03:13 +0300

    updated MovieGenreRepository FilterMoviesByGenre method

--- movies-api/Repository/MovieGenreRepository.cs
+++ movies-api/Repository/MovieGenreRepository.cs
@@ -27,18 +27,16 @@ namespace movies_api.Repository
         public async Task<List<Movie>> FilterMoviesByGenre(QueryObject query)
         {
             var skipNumber = (query.PageNumber - 1) * query.PageSize;
-            var movies = await _context.MovieGenre
-            .Where((mg) => mg.Genre.GenreName.ToLower() == query.GenreName.ToLower())
-            .Select((movie) => new Movie
-            {
-                Id = movie.Movie.Id,
-                Title = movie.Movie.Title,
-                Description = movie.Movie.Description,
-                Genres = movie.Movie.Genres
-            })
-            .Skip(skipNumber)
-            .Take(query.PageSize)
-            .ToListAsync();
+            var movies = await _context.Movie
+        .Where(m => m.MovieGenre
+            // Checks if there is at least one element in a collection that matches a condition.
+            // returns true or false
+            .Any(mg => mg.Genre.GenreName.ToLower() == query.GenreName.ToLower()))
+        .Include(mg => mg.MovieGenre)          
+            .ThenInclude(m => m.Genre)     
+        .Skip(skipNumber)
+        .Take(query.PageSize)
+        .ToListAsync();
             return movies;
         }
     }
