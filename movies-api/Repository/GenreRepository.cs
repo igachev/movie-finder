c6ab612f0c72506ff3937c13470a021e42855d8b
igachev <ivophoenix1135@gmail.com>
2025-09-17 12:57:33 +0300

    updated GenreRepository.Implemented methods:GetGenreId,IsGenreExist

--- movies-api/Repository/GenreRepository.cs
+++ movies-api/Repository/GenreRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using movies_api.Data;
 using movies_api.Interfaces;
 using movies_api.Models;
@@ -20,6 +21,25 @@ namespace movies_api.Repository
             await _context.Genre.AddAsync(genre);
             await _context.SaveChangesAsync();
             return genre;
+        }
+
+        public async Task<int> GetGenreId(Genre genre)
+        {
+            var genreExist = await _context.Genre.FirstOrDefaultAsync((g) => g.GenreName == genre.GenreName);
+            return genreExist.Id;
+        }
+
+        public async Task<bool> IsGenreExist(Genre genre)
+        {
+            var genreExist = await _context.Genre.FirstOrDefaultAsync((g) => g.GenreName == genre.GenreName);
+            if (genreExist != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
