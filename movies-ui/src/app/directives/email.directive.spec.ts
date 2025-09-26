b98104476ec08b1d8eef02ecd4941178adf459fc
igachev <ivophoenix1135@gmail.com>
2025-09-26 16:53:39 +0300

    added tests for emailDirective

--- movies-ui/src/app/directives/email.directive.spec.ts
+++ movies-ui/src/app/directives/email.directive.spec.ts
@@ -1,8 +1,40 @@
+import { AbstractControl, FormControl } from '@angular/forms';
 import { EmailDirective } from './email.directive';
 
 describe('EmailDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EmailDirective();
-    expect(directive).toBeTruthy();
-  });
+
+  test("should have 'emailRegex' property",async() => {
+    const emailDirective = new EmailDirective();
+    expect(emailDirective.emailRegex).toBeDefined()
+  })
+
+  test("'emailRegex' property should be of type RegExp",async() => {
+    const emailDirective = new EmailDirective();
+    expect(emailDirective.emailRegex).toBeInstanceOf(RegExp)
+  })
+
+  test("should have 'validate' property",async() => {
+    const emailDirective = new EmailDirective();
+    expect(emailDirective.validate).toBeDefined()
+  })
+
+   test("should have 'validate' property of type Function",async() => {
+    const emailDirective = new EmailDirective();
+    expect(emailDirective.validate).toBeInstanceOf(Function)
+  })
+
+    test("should return { invalidEmail: {invalid: true} } object when the email format is invalid",async() => {
+    const emailDirective = new EmailDirective();
+    const mockAbstractControl: AbstractControl = new FormControl("ivo@")
+    const emailNotValid = emailDirective.validate(mockAbstractControl)
+    expect(emailNotValid).toEqual({ invalidEmail: {invalid: true} })
+  })
+
+    test("should return null when the email format is valid",async() => {
+    const emailDirective = new EmailDirective();
+    const mockAbstractControl: AbstractControl = new FormControl("ivo@abv.bg")
+    const validEmail = emailDirective.validate(mockAbstractControl)
+    expect(validEmail).toBe(null)
+  })
+
 });
