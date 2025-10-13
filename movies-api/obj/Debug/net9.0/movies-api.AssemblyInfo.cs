a5cc2355fff4a6e06e9c43829cbe93e5a6b5293e
igachev <ivophoenix1135@gmail.com>
2025-10-13 14:59:35 +0300

    created tests for MovieService.getMovies method

--- movies-api/obj/Debug/net9.0/movies-api.AssemblyInfo.cs
+++ movies-api/obj/Debug/net9.0/movies-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("movies-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e824fe85788a75e2513941952441a2719a419bc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22218d7b17cb28d103b341ea5b65ab413b2278fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("movies-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("movies-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- movies-ui/src/app/services/movie.service.spec.ts
+++ movies-ui/src/app/services/movie.service.spec.ts
@@ -1,16 +1,59 @@
 import { TestBed } from '@angular/core/testing';
-
 import { MovieService } from './movie.service';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { Movie } from '../types/MovieTypes';
 
 describe('MovieService', () => {
-  let service: MovieService;
+
+  const mockMovies: Movie[] = [
+    { id: 1, title: 'Batman', description: 'batman description', comments: [], genres: [{ genreName: "action" }, { genreName: "drama" }], firstImg: 'img1' },
+    { id: 2, title: 'Avatar', description: 'avatar description', comments: [], genres: [{ genreName: "sci-fi" }], firstImg: 'img2' },
+    { id: 3, title: 'Star Wars:Rogue One', description: 'star wars description', comments: [], genres: [{ genreName: "sci-fi" }, { genreName: "action" }], firstImg: 'img3' },
+  ];
+
+  let httpClientMock = { get: jest.fn() };
+  let movieService: MovieService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MovieService);
+     httpClientMock = { get: jest.fn() };
+
+    TestBed.configureTestingModule({
+      providers: [
+        // Registers that service with the TestBed’s dependency system
+        MovieService,
+        { provide: HttpClient, useValue: httpClientMock }
+      ]
+    });
+    // Actually retrieves the instance so you can use it in your test
+    movieService = TestBed.inject(MovieService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
+
+  test("'getMovies' method should be defined",() => {
+    expect(movieService.getMovies).toBeDefined();
+  })
+
+  test("'getMovies' should return correct movies based on pagination parameters", (done) => {
+    const pageNumber = 1;
+    const pageSize = 2;
+    let start = pageNumber - 1 * pageSize;
+    let end = pageSize + start;
+
+    // Mock the HttpClient.get call
+    httpClientMock.get.mockReturnValue(of(mockMovies.slice(start,end)));
+    const expectedMovies = mockMovies.slice(start,end);
+    movieService.getMovies(pageNumber, pageSize).subscribe(movies => {
+      expect(movies).toEqual(expectedMovies);
+      expect(httpClientMock.get).toHaveBeenCalledTimes(1);
+      expect(movies.length).toBe(expectedMovies.length);
+      done();
+    });
   });
+
+
+
 });
